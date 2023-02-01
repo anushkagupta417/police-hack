dcc9ec5251cc63a8762da2c79727df9bb9785c1d
anushkagupta417 <76914779+anushkagupta417@users.noreply.github.com>
2023-02-01 22:39:15 +0530

    Update admin_dashboard.dart

--- frontend/lib/dashboards/admin_dashboard.dart
+++ frontend/lib/dashboards/admin_dashboard.dart
@@ -1,16 +1,128 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:ksp_hack/views/addNewUser.dart';
 
-class AdminDash extends StatefulWidget {
-  const AdminDash({Key? key}) : super(key: key);
-
+import 'home_page.dart';
+class Dashboard extends StatefulWidget {
   @override
-  State<AdminDash> createState() => _AdminDashState();
+  _DashboardState createState() => _DashboardState();
 }
 
-class _AdminDashState extends State<AdminDash> {
+class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color.fromRGBO(49, 87, 110, 1.0),
+        title: Text('Admin Dashboard'),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => const HomePage()));
+            },
+            icon: const Icon(
+              Icons.home,
+              size: 20,
+              color: Colors.grey,
+            )),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 2.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(3.0),
+          children: <Widget>[
+            addUser("Add User", Icons.person_add),
+            removeUser("Remove User", Icons.delete),
+            // makeDashboardItem("Remove User", Icons.delete),
+            // makeDashboardItem("Alphabet", Icons.alarm),
+            // makeDashboardItem("Alphabet", Icons.alarm),
+            // makeDashboardItem("Alphabet", Icons.alarm),
+            // makeDashboardItem("Alphabet", Icons.alarm)
+          ],
+        ),
+      ),
+    );
   }
+}
+Card addUser(String title, IconData icon) {
+  return Card(
+      elevation: 1.0,
+      margin: EdgeInsets.all(120.0),
+      child: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
+        child:  Builder(
+          builder: (context) {
+            return InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => addNewUser()));
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                verticalDirection: VerticalDirection.down,
+                children: <Widget>[
+                  SizedBox(height: 50.0),
+                  Center(
+                      child: Icon(
+                        icon,
+                        size: 40.0,
+                        color: Colors.black,
+                      )),
+                  SizedBox(height: 20.0),
+                  Center(
+                    child:  Text(title,
+                        style:
+                        TextStyle(fontSize: 18.0, color: Colors.black)),
+                  )
+                ],
+              ),
+            );
+          }
+        ),
+      ));
+}
+
+Card removeUser(String title, IconData icon) {
+  return Card(
+      elevation: 1.0,
+      margin: EdgeInsets.all(120.0),
+      child: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
+        child:  Builder(
+            builder: (context) {
+              return InkWell(
+                onTap: () {
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  verticalDirection: VerticalDirection.down,
+                  children: <Widget>[
+                    SizedBox(height: 50.0),
+                    Center(
+                        child: Icon(
+                          icon,
+                          size: 40.0,
+                          color: Colors.black,
+                        )),
+                    SizedBox(height: 20.0),
+                    Center(
+                      child:  Text(title,
+                          style:
+                          TextStyle(fontSize: 18.0, color: Colors.black)),
+                    )
+                  ],
+                ),
+              );
+            }
+        ),
+      ));
 }
